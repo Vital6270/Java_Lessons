519f942b1e91f2dce01ad2d4bd30291329d57c6d
Vitaliy <116778179+Vital6270@users.noreply.github.com>
2024-10-14 17:55:44 +0300

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,33 +1,51 @@
-// Задание 2. Создать массив из 5 сотрудников. Пример: вначале объявляем массив объектов
-// Person[] persArray = new Person[5]; потом для каждой ячейки массива задаем объект
-// persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
-// persArray[1] = new Person(...); ... persArray[4] = new Person(...);
+// Задание 1. Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
+// Конструктор класса должен заполнять эти поля при создании объекта. Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.
 
-public class Main {
-    Park park1 = new Park("Great", 5.8,15);
-    Park.Attraction attr1 = new Park.Attraction("Карусель", "09.00 - 18.00", 2.5);
-    Park.Attraction attr2 = new Park.Attraction("Лодочка", "10.00 - 18.00", 1.5);
+public class Employee {
 
-    public static void main(String[] args) {
+    private String name;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
 
-        Employee[] persArray = new Employee[5];
-        persArray[0] = new Employee("Ivanov Ivan", "Engineer",
-                "ivivan@mailbox.com", "892312312", 30000, 30);
-        persArray[1] = new Employee("Sidorov Petr", "Tester",
-                "sidorov@mailbox.com", "872315348", 20000, 40);
-        persArray[2] = new Employee("Grebenko Evgeni", "Teacher",
-                "evgeni1@mailbox.com", "872315348", 24000, 35);
-        persArray[3] = new Employee("Petrova Irina", "Student",
-                "irinap@mailbox.com", "872315348", 26000, 18);
-        persArray[4] = new Employee("Avdeenko Petr", "Driver",
-                "avdeenkop@mailbox.com", "872315348", 10000, 54);
+    public Employee(String name, String position, String email, String phoneNumber, int salary, int age){
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
 
-        Employee employee1 = new Employee("Anisimov Petr", "Tester",
-                "anp1@mailbox.com", "872310044", 12000, 19);
+    public String getName() {
+        return name;
+    }
 
-        // проверяем работу метода
-        employee1.getEmployeeInfo(employee1);
-        persArray[2].getEmployeeInfo(persArray[2]);
+    public String getPosition() {
+        return position;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void getEmployeeInfo (Employee employee){
+        System.out.println("Employee's name is: " + employee.getName() + "\nAge: " + employee.getAge()
+                + "\nPosition: " + employee.getPosition() + "\nEmail: " + employee.getEmail() + "\nPhone number: " +
+                employee.getPhoneNumber() + "\nSalary: " + employee.getSalary());
     }
 }
