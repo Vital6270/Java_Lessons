d76aa062cc335811ee86f739a779f26e4e0c34cf
Vitaliy <116778179+Vital6270@users.noreply.github.com>
2024-10-20 21:02:18 +0300

    Create PhoneBook.java

---
+++ PhoneBook.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// 2. Написать простой класс Телефонный Справочник, который хранит в себе список фамилий
+// и телефонных номеров. В этот телефонный справочник с помощью метода add() можно
+// добавлять записи, а с помощью метода get() искать номер телефона по фамилии.
+// Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев),
+// тогда при запросе такой фамилии должны выводиться все телефоны.
+public class PhoneBook {
+    private Map<String, List<String>> book;
+
+    public PhoneBook() {
+        book = new HashMap<>();
+    }
+
+    // Метод для добавления записи
+    public void add(String lastName, String phoneNumber) {
+        book.putIfAbsent(lastName, new ArrayList<>());
+        book.get(lastName).add(phoneNumber);
+    }
+
+    // Метод для получения номеров по фамилии
+    public List<String> get(String lastName) {
+        return book.getOrDefault(lastName, new ArrayList<>());
+    }
+}
